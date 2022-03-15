ff4da761639321891560d35ad97307b6cdc1dd5a
moralessf <fcesar_moan@hotmail.com>
2022-03-14 22:40:03 -0600

    Actualización de íconos

--- main.js
+++ main.js
@@ -18,32 +18,24 @@ const construirTarea = () => {
 
    const tarea =`<div class="border border d-flex  justify-content-between bg-white">
             <div class="">`;
-    if(tarea.estado == "pendiente"){
-               tarea+= `<svg id = "pendiente" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>`
+    if(tarea.estado == "pendiente")
+    {
+        tarea+= `<i id = "pendiente" class="fa-solid fa-list-check"></i>`;
     }
     else
     {
 
-        tarea+= `</svg> <svg id = "completa" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
-        </svg>` 
+        tarea+= `<i id = "completa" class="bi bi-check-lg"></i>`;
     }
 
     tarea+= `${tarea.nombre}
             </div> 
             <div>
-                <button class="editar" onclick="editarActividad(${tarea.id})">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="blue" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                    </svg>
+                <button class="editar btn btn-outline-primary" onclick="editarActividad(${tarea.id})">
+                    <i class="bi bi-pencil-square"></i>
                 </button>
-                <button class="eliminar" onclick="eliminarActividad(${tarea.id})">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
+                <button class="eliminar btn btn-outline-danger" onclick="eliminarActividad(${tarea.id})">
+                    <i class="bi bi-trash-fill"></i>
                 </button>
             </div>
     </div>`;
@@ -51,6 +43,9 @@ const construirTarea = () => {
     return tarea;
         
 }
+
+//<i class="bi bi-pencil-square"></i>
+//<i class="bi bi-trash-fill"></i>
 
 // const itemActividad = (actividad) => {
 //     const card = `<div class="card col-sm-4">
